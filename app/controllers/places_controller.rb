d03e1218a1819f848590993adca1d97e414db194
JESSE DESANTO <JESSESUS13@GMAIL.COM>
2018-05-14 09:31:33 -0700

    Add destroy function

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -22,22 +22,26 @@ class PlacesController < ApplicationController
   def edit
     @place = Place.find(params[:id])
     if @place.user != current_user
-      return render plain: 'Not Allowed', status :forbidden
-      end
+      return render plain: 'Not Allowed', status: :forbidden
+    end
   end
 
   def update
     @place = Place.find(params[:id])
 
     if @place.user != current_user
-      return render plain: 'Not Allowed', status :forbidden
-      end
-    Place.update_attributes(place_params)
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
+    @place.update_attributes(place_params)
     redirect_to root_path
   end
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end  
     @place.destroy
     redirect_to root_path
   end
