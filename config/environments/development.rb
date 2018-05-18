ec1a7c446b4ce7f4b72cdbe68f59a92e7a8ae637
JESSE DESANTO <JESSESUS13@GMAIL.COM>
2018-05-18 09:11:25 -0700

    Adjust aoutomated email mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gamil.com",
+    address: "smtp.gmail.com",
     port: 587,
     domain: "welpapp.com",
     authentication: "plain",
